07a9eea11f3c6f03c0ee6aadc59c6bb8f49e6df7
hagimori <shogo-hagimori@exa-corp.co.jp>
2017-09-28 14:40:17 +0900

    ユーザー情報の収集コードの追加

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var request = require('request')
 var restify = require('restify');
 var builder = require('botbuilder');
+var WebClient = require('@slack/client').WebClient;
 const loger = require('./log.js');
 
 //=========================================================
@@ -21,7 +22,7 @@ var connector = new builder.ChatConnector({
 server.post('/', connector.listen()); // 例：https://xxx.co.jp/
 // server.post('/message/api', connector.listen()); 例：https://xxx.co.jp/message/api
 
-var bot = module.exports = new builder.UniversalBot(connector, [
+var mbfBot = module.exports = new builder.UniversalBot(connector, [
     (session, args, next) => {
     },
     (session, res, next) => {
@@ -44,22 +45,29 @@ var bot = module.exports = new builder.UniversalBot(connector, [
         }
     },
 ]);
-bot.library(require('./Cards').createLibrary());
+// bot.library(require('./Cards').createLibrary());
 
-bot.on('conversationUpdate', function (message) {
+var slack = new WebClient(process.env.SLACK_BOT_TOKEN);
+
+mbfBot.on('conversationUpdate', function (message) {
     if (message.membersAdded) {
         loger.log("join Member", message);
+        var memberInfo = getMemberInfo(message.membersAdded);
+        slack.chat.postMessage(process.env.USER_INFO_DESTINATION, memberInfo, (err, res) => {
+            loger.console('sent: ', res);
+        })
+        memberInfo.start = getTimeStamp();
         var membersAdded = message.membersAdded
             .map((m) => {
                 var isSelf = m.id === message.address.bot.id;
                 return (isSelf ? message.address.bot.name : m.name);
             })
             .join(', ');
-
         var reply = new builder.Message()
             .address(message.address)
             .text('いらっしゃいませー ' + membersAdded + ' さん');
-        bot.send(reply);
+
+        mbf.send(reply);
     }
     if (message.membersRemoved) {
         loger.log("join Member", message);
@@ -72,6 +80,27 @@ bot.on('conversationUpdate', function (message) {
         var reply = new builder.Message()
             .address(message.address)
             .text('ばいばーい' + membersRemoved);
-        bot.send(reply);
+        mbfBot.send(reply);
     }
 });
+
+function makeMemberInfo(userMap, timeStampKey) {
+    userMap.map((m) => {
+        m.id = m.id.split(":")[0]
+        m.email = slack.users.info(m.id, (err, res) => {
+            return res.user.prfile.email;
+        })
+    })
+    return userMap
+}
+
+function getTimeStamp() {
+    var d = new Date();
+    var year = d.getFullYear();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
+    var hour = (d.getHours() < 10) ? '0' + d.getHours() : d.getHours();
+    var min = (d.getMinutes() < 10) ? '0' + d.getMinutes() : d.getMinutes();
+    var sec = (d.getSeconds() < 10) ? '0' + d.getSeconds() : d.getSeconds();
+    return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
+}
